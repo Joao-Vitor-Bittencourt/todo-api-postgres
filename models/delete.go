a6105cdcd446a3f5a5ef9f9b842b5bfdf0b38295
João Bittencourt <joaobittencourtd3v@gmail.com>
2023-08-16 20:20:30 -0300

    creating delete.go

--- models/delete.go
+++ models/delete.go
@@ -2,17 +2,15 @@ package models
 
 import "pratica.com/api-postgresql/db"
 
-func Update(id int64, todo Todo) (int64, error) {
+func DElete(id int64, todo Todo) (int64, error) {
 	connection, err := db.OpenConnection()
-
 	if err != nil {
 		return 0, err
 	}
 
 	defer connection.Close()
 
-	res, err := connection.Exec(`UPDATE todos SET title=$2, description=$3, done=$4 WHERE id=$1`, id, todo.Title, todo.Description, todo.Done)
-
+	res, err := connection.Exec(`DELETE FROM todos WHERE id=$1`, id)
 	if err != nil {
 		return 0, err
 	}
